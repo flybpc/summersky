42edb7e8e62e00735b606876ac21837f1a5731e3
flybpc <liwenjjj@gmail.com>
2017-01-07 16:56:45 +0800

    重构代码

---
+++ core/src/main/java/com/flybpc/summersky/core/app.java
@@ -0,0 +1,10 @@
+package com.flybpc.summersky.core;
+
+/**
+ * Created by WenJie on 2017/1/7.
+ */
+public class app {
+    public static void main(String[] args){
+      System.out.print("hello word");
+    }
+}
