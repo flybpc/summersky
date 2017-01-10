3c26255ed6e79e4cf3d6d15a10db5b198fd58d53
flybpc <liwenjjj@gmail.com>
2017-01-10 15:54:39 +0800

    增加spring cloud eureka

--- core/src/main/java/com/flybpc/summersky/core/app.java
+++ core/src/main/java/com/flybpc/summersky/core/app.java
@@ -3,8 +3,19 @@ package com.flybpc.summersky.core;
 /**
  * Created by WenJie on 2017/1/7.
  */
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableEurekaServer
+@SpringBootApplication
 public class app {
-    public static void main(String[] args){
-      System.out.print("hello word");
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(app.class).web(true).run(args);
     }
+
 }
