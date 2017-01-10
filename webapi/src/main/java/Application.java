d807e8cc00f795bc7a108119fd589c72c2c42303
flybpc <liwenjjj@gmail.com>
2017-01-10 17:17:28 +0800

    增加spring cloud  config 和hystrix

--- webapi/src/main/java/Application.java
+++ webapi/src/main/java/Application.java
@@ -1,11 +1,12 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by luyh on 16/4/27.
  */
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan("com.flybpc.summersky")
 public class Application {
