74788f1bbbc34b2295dc09d2ffeb5d2d914b5fac
flybpc <liwenjjj@gmail.com>
2017-01-12 11:13:46 +0800

    增加spring cloud  config 和hystrix

--- core/src/main/java/com/flybpc/summersky/core/app.java
+++ core/src/main/java/com/flybpc/summersky/core/app.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 @SpringBootApplication
 public class app {
